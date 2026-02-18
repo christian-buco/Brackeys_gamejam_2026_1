8eef7891276676a7c98d3d0348e10e383efa887c
JustBacon <christianbuco17@gmail.com>
2026-02-18 09:17:11 -0800

    crush mechanic work

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,15 +3,22 @@ extends CharacterBody2D
 @export var speed: float = 100.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+# Crush mechanic
+var overlapping_walls := []
+@export var crush_grace_time: float
+var crush_timer: float = 0.0
+var is_currently_crushed: bool = false
+
 var can_move := true
 
+# screen shake
+var shake_strength := 0.0
 
 func set_movement_enabled(enabled: bool) -> void:
 	can_move = enabled
 	if not enabled:
 		velocity = Vector2.ZERO
 		animated_sprite.play("idle")
-
 
 func _physics_process(delta: float) -> void:
 	if not can_move:
@@ -28,11 +35,20 @@ func _physics_process(delta: float) -> void:
 
 	velocity = input_vector * speed
 	move_and_slide()
+	check_crush(delta)
 	
 	if input_vector != Vector2.ZERO:
 		play_walk_animation(input_vector)
 	else:
 		animated_sprite.play("idle")
+
+func _process(delta) -> void:
+	if shake_strength > 0:
+		shake_strength = lerp(shake_strength, 0.0, 10 * delta)
+		$Camera2D.offset = Vector2(
+			randf_range(-shake_strength, shake_strength),
+			randf_range(-shake_strength, shake_strength)
+		)
 
 func play_walk_animation(direction: Vector2):
 	if abs(direction.x) > abs(direction.y):
@@ -45,3 +61,47 @@ func play_walk_animation(direction: Vector2):
 			animated_sprite.play("walk_down")
 		else:
 			animated_sprite.play("walk_up")
+
+func check_crush(delta):
+
+	if overlapping_walls.is_empty():
+		crush_timer = 0.0
+		return
+	
+	for wall in overlapping_walls:
+		if wall.is_moving and not wall.is_paused:
+			crush_timer += delta
+			
+			if crush_timer >= crush_grace_time:
+				die()
+			return
+	crush_timer = 0.0
+
+func die():
+	if not can_move:
+		return
+
+	can_move = false
+	velocity = Vector2.ZERO
+
+	print("CRUSHED")
+	animated_sprite.scale = Vector2(0.6, 1.2)
+	shake_strength = 8.0
+
+	# Optional: small freeze effect
+	Engine.time_scale = 0.1
+	await get_tree().create_timer(0.15).timeout
+	Engine.time_scale = 1.0
+
+
+	get_tree().reload_current_scene()
+
+
+func _on_crush_detector_body_entered(body: Node2D) -> void:
+	if body.is_in_group("moving_wall"):
+		overlapping_walls.append(body)
+
+
+func _on_crush_detector_body_exited(body: Node2D) -> void:
+	if body in overlapping_walls:
+		overlapping_walls.erase(body)
