73c576cefa053d86f13ed0ad0eab577017282392
rezmayyy <139288353+rezmayyy@users.noreply.github.com>
2026-02-18 18:09:43 -0800

    Add transition to main state when play is CLICKED

--- scripts/menu_state.gd
+++ scripts/menu_state.gd
@@ -5,34 +5,37 @@ extends Control
 
 func _ready() -> void:
 	menu.visible = true
-	panel.visible = false 
-	
+	panel.visible = false
+	$Transition.fade_in()
+	$Transition.transitioned.connect(_on_transition_transitioned)
+
 func _on_controls_pressed() -> void:
 	menu.visible = false
 	panel.visible = true
 
 
-# Called when Play is pressed
 func _on_play_pressed():
-	# Replace with your state switching logic
-	print("Start the game")
+	$Transition.transition()
+
+
+func _on_transition_transitioned() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
 
 # Called when Controls is pressed
 func _on_ControlsButton_pressed():
 	print("Show controls menu")
-	get_tree().change_scene("res://ControlsState.tscn") # or open a popup
+	get_tree().change_scene("res://ControlsState.tscn")
 
 # Called when Credits is pressed
 func _on_CreditsButton_pressed():
 	print("Show credits")
-	get_tree().change_scene("res://CreditsState.tscn") # or open a popup
+	get_tree().change_scene("res://CreditsState.tscn")
 
 # Called when Quit is pressed
 func _on_QuitButton_pressed():
 	get_tree().quit()
-	
 
 
 func _on_back_pressed() -> void:
-	_ready() 
+	_ready()
--- scripts/transition.gd
+++ scripts/transition.gd
@@ -2,11 +2,14 @@ extends CanvasLayer
 
 signal transitioned
 
-func transition(): 
+func transition() -> void:
 	$AnimationPlayer.play("fade_to_black")
+
+
+func fade_in() -> void:
+	$AnimationPlayer.play("fade_to_normal")
+
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "fade_to_black":
 		emit_signal("transitioned")
-		$AnimationPlayer.play("fade_to_normal")
-		
